7a3ac24c3b408e25314908c156ac2bc21d944546
asif2211 <asifbsit@gmail.com>
2021-01-14 16:30:45 +0500

    add duck list actions , reducers

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
-import { View ,Text} from 'react-native'
+import { View, Text } from 'react-native'
 
 export default class DeckList extends Component {
     render() {
         return (
-            <View>
-                <Text>
-                Deck list
-                </Text>
-            </View>
+           <View>
+               <Text>DeckList</Text>
+           </View>
         )
     }
 }
